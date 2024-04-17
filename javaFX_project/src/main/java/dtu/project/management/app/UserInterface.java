9763a5f8d24ca98b25dfd4b23814f6bbc98834f6
WeihaoMo1 <weihaomo520@gmail.com>
2024-04-17 14:42:01 +0200

    Update Userinterface (Create Project"
    Created main menu and user interface for creation of project and see projects

--- javaFX_project/src/main/java/dtu/project/management/app/UserInterface.java
+++ javaFX_project/src/main/java/dtu/project/management/app/UserInterface.java
@@ -2,6 +2,8 @@ package dtu.project.management.app;
 
 import java.util.Scanner;
 
+import dtu.project.management.domain.Project;
+
 public class UserInterface {
 	
 	private ProjectManagementApp projectManagementApp = new ProjectManagementApp();
@@ -10,13 +12,56 @@ public class UserInterface {
 	public UserInterface() throws OperationNotAllowedException {
 		projectManagementApp.setup();
 		System.out.println("Welcome!");
+		runApp();
+	}
+	
+	public void runApp() throws OperationNotAllowedException {
 		login();
+		mainMenu();
+	}
+	
+	public void mainMenu() {
+		System.out.println();
+		System.out.println("Input 1 to create project");
+		System.out.println("Input 2 to see all projects");
+		System.out.println("Input 0 to close the program");
+		int input = console.nextInt();
+		switch (input) {
+		case 1: 
+			createProject();
+		case 2: 
+			showProjects();
+		case 0:
+			System.exit(0);
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + input);
+		}
+		
 	}
 	
 	public void login() throws OperationNotAllowedException {
 		System.out.print("Input login credentials: ");
 		String input = console.next();
 		projectManagementApp.login(input);
-		System.out.println(projectManagementApp.getCurrentLogin().getId());
+		System.out.println("You are now logged in as: " + projectManagementApp.getCurrentLogin().getId());
 	}
+	
+	public void createProject(){
+		System.out.print("Input project name: ");
+		String input = console.next();
+		projectManagementApp.createProject(input);
+		projectManagementApp.addProject();
+		System.out.println("Created project: " + projectManagementApp.getProject().getName() + " " + projectManagementApp.getProject().getSerialnumber());
+		mainMenu();
+	}
+	
+	public void showProjects() {
+		System.out.println("List of projects:");
+		for(Project p: projectManagementApp.getProjects()) {
+			System.out.println(p.getSerialnumber() + " " + p.getName() );
+		}
+		mainMenu();
+	}
+	
+	
 }
